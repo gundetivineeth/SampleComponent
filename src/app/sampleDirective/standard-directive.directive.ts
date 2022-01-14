c47fddb574f28a9817a44c0bf590c0085779f617
gundetivineeth <vineethg508@gmail.com>
2022-01-14 12:32:11 -0700

    Adding HostListener example

--- src/app/sampleDirective/standard-directive.directive.ts
+++ src/app/sampleDirective/standard-directive.directive.ts
@@ -1,4 +1,4 @@
-import { Directive, ElementRef, OnInit, Renderer2 } from '@angular/core';
+import { Directive, ElementRef, HostListener, OnInit, Renderer2 } from '@angular/core';
 
 @Directive({
   selector: '[appStandardDirective]'
@@ -6,8 +6,16 @@ import { Directive, ElementRef, OnInit, Renderer2 } from '@angular/core';
 export class StandardDirectiveDirective implements OnInit{
 
   constructor(private elementRef:ElementRef, private render:Renderer2) { }
+  
   ngOnInit(): void {
+    //this.render.setStyle(this.elementRef.nativeElement,"background-color","blue");
+  }
+
+  @HostListener('mouseenter') mouseover(eventData:Event){
     this.render.setStyle(this.elementRef.nativeElement,"background-color","blue");
   }
 
+  @HostListener('mouseleave') mouseleave(eventData:Event){
+    this.render.setStyle(this.elementRef.nativeElement,"background-color","transparent");
+  }
 }
